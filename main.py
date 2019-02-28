6dd7ac960c8efdd3f73100e45dc193e587816efd
Kooldeji <dejiakande33@gmail.com>
2019-02-28 19:45:57 +0100

    init

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+
 def inp():
-    pass
+    fname = "a.txt"
+    file = open(fname)
+    n = int(file.readline())
+    v_photos = []
+    h_photos = []
+    for i in range(n):
+        line = file.readline().strip().split(" ")
+        s = line[0]
+        tags = line[2:]
+        if s == "H":
+            h_photos.append(Photo(i, s, tags))
+        else:
+            v_photos.append(Photo(i, s, tags))
+    solve(photos)
+inp()
