bba81c50b587a39d403f2f468655280608db3c3b
Timilehin <phillzmike@gmail.com>
2019-02-28 20:29:19 +0100

    Done with Solve

--- timilehin.py
+++ timilehin.py
@@ -1,23 +1,38 @@
 
-sol = dict()
-memoTable = dict()
+
+
 
 
 def solve(allPosSlides):
 
+    sol = [-1 for i in range(len(allPosSlides))]
+    memoTable = [-1 for i in range(len(allPosSlides))]
+
     def getBest(allPosSlides, i, formerScore):
+
+        if memoTable[i] != -1:
+            return memoTable[i]
 
         if i == len(allPosSlides):
             return formerScore
 
-        score = 0
+        score = formerScore
         maxScore = score
         for j in range(i, len(allPosSlides) - 1):
-            
-            formerScore += getScore(allPosSlides[j], allPosSlides[j + 1])
+
+            formerScore += getScore(allPosSlides[i], allPosSlides[j + 1])
             score = getBest(allPosSlides, j, formerScore)
             if score > maxScore:
+                sol[j+1] = i
                 maxScore = score
 
         return maxScore
+
+    stack = []
+    pos = len(allPosSlides - 1)
+    while pos != 0:
+        stack.append(allPosSlides[pos])
+        pos = sol[pos]
+    return stack
+
 
