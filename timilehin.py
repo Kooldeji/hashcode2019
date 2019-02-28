8cb17eba804bfe989118625f8d9c9ffe32c6538f
Timilehin <phillzmike@gmail.com>
2019-02-28 20:11:47 +0100

    init_solve

--- timilehin.py
+++ timilehin.py
@@ -1,5 +1,23 @@
 
+sol = dict()
+memoTable = dict()
+
+
 def solve(allPosSlides):
 
-    def getBest(allPosSlides, i):
-        pass
+    def getBest(allPosSlides, i, formerScore):
+
+        if i == len(allPosSlides):
+            return formerScore
+
+        score = 0
+        maxScore = score
+        for j in range(i, len(allPosSlides) - 1):
+            
+            formerScore += getScore(allPosSlides[j], allPosSlides[j + 1])
+            score = getBest(allPosSlides, j, formerScore)
+            if score > maxScore:
+                maxScore = score
+
+        return maxScore
+
