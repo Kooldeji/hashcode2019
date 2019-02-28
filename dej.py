9fe042e52d90b192517c569cacba23c16d5580a9
Timilehin <phillzmike@gmail.com>
2019-02-28 21:29:04 +0100

    .

--- dej.py
+++ dej.py
@@ -1,8 +1,41 @@
-# def solve1(slides):
-#     slides = set(slides)
-#     for slide in slides:
-#         slides.remove(slide)
-#         score(gen(slide, slides))
-#
-# def gen(slide, slides):
-#
+from jedd import getScore
+
+
+def solve1(slides):
+    slides = set(slides)
+    max_s = 0
+    max_slides = None
+    for slide in slides:
+        slides.remove(slide)
+        s = getTotalScore(gen(slide, slides))
+        slides.add(slide)
+        if s > max_s:
+            max_s = s
+            max_slides = slides
+    return max_slides
+
+
+
+def gen(slide, slides):
+    if len(slides) == 2:
+        return [slides[0], slides[1]]
+    lst = []
+    max_s = 0
+    max_slides = None
+    lst.append(slide)
+    for slide1 in slides:
+        slides.remove(slide1)
+        s = getScore(slide, slide1) + getTotalScore(gen(slide1, slides))
+        slides.add(slide1)
+        if s > max_s:
+            max_s = s
+            max_slides = slides
+    lst.extend(max_slides)
+    return max_slides
+
+
+def getTotalScore(slides):
+    sum = 0
+    for i in range(0, len(slides)):
+        sum = getScore(slides[i], slides[i+1])
+    return sum
